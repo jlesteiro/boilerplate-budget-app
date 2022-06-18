4227398915e44706181835e23ddf11f11c559d41
“jlesteiro” <joseangellt96@gmail.com>
2022-06-17 21:06:11 -0400

    tests ok

--- budget.py
+++ budget.py
@@ -80,7 +80,7 @@ def create_spend_chart(categories):
                 histogram += "   "
             else:
                 histogram += " o "
-        histogram += "\n"
+        histogram += " \n"
 
     histogram += " " * 4 + "---" * len(categories) + "-"
     
@@ -99,27 +99,4 @@ def create_spend_chart(categories):
         for name in new_names:
             histogram += name[i:i+1] + " " * 2
     
-    return histogram
-
-
-
-food = Category("Food")
-entertainment = Category("Entertainment")
-business = Category("Business")
-
-food.deposit(900, "deposit")
-entertainment.deposit(900, "deposit")
-business.deposit(900, "deposit")
-food.withdraw(105.55)
-entertainment.withdraw(33.40)
-business.withdraw(10.99)
-actual = create_spend_chart([business, food, entertainment])
-
-expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
-
-
-print("#################################################")
-print(repr(actual))
-print("#################################################")
-print(repr(expected))
-print("#################################################")
+    return histogram 
